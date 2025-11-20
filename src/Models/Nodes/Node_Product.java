9453fe276b384e12016c9db625eac5ad4479fc9a
Aileth Oquendo <ailethcantero@gmail.com>
2025-11-19 22:55:51 -0500

    Updating the products node.

--- src/Models/Nodes/Node_Product.java
+++ src/Models/Nodes/Node_Product.java
@@ -1,5 +1,40 @@
 package Models.Nodes;
 
+import Models.Product;
+
 public class Node_Product {
     
+    private Product product;
+    private Node_Product next;
+    private Node_Product former;
+
+    public Node_Product(Product product) {
+        this.product = product;
+        this.next = null;
+        this.former = null;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Node_Product getNext() {
+        return next;
+    }
+
+    public void setNext(Node_Product next) {
+        this.next = next;
+    }
+
+    public Node_Product getFormer() {
+        return former;
+    }
+
+    public void setFormer(Node_Product former) {
+        this.former = former;
+    }        
 }
--- src/Models/Product.java
+++ src/Models/Product.java
@@ -1,5 +1,71 @@
 package Models;
 
 public class Product {
-        
+
+    private int idProduct;
+    private String name;
+    private String brand;
+    private String[] sizes;
+    private float price;
+    private String description;
+
+    public Product() {
+    }
+
+    public Product(int idProduct, String name, String brand, String[] sizes, float price, String description) {
+        this.idProduct = idProduct;
+        this.name = name;
+        this.brand = brand;
+        this.sizes = sizes;
+        this.price = price;
+        this.description = description;
+    }
+
+    public int getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(int idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String[] getSizes() {
+        return sizes;
+    }
+
+    public void setSizes(String[] sizes) {
+        this.sizes = sizes;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }        
 }
