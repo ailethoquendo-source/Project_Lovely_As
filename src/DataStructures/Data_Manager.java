5ccb8a0387f10d15d5e0c07e300d7a0882ce8262
Alejandra Pinto <aleja.pintoro333@gmail.com>
2025-11-20 23:04:35 -0500

    Updating the Data_Manager class so that it correctly implements the Singleton design pattern and can manage instances of different data structures.

--- src/DataStructures/Data_Manager.java
+++ src/DataStructures/Data_Manager.java
@@ -1,5 +1,28 @@
 package DataStructures;
 
 public class Data_Manager {
-    
+
+    private static Data_Manager manager;
+    private final List_Double_User list_user;
+    private final Stack_Of_Product stack_Product;
+
+    public Data_Manager() {
+        this.list_user = new List_Double_User();
+        this.stack_Product = new Stack_Of_Product();
+    }
+
+    public static Data_Manager getManager() {
+        if (manager == null) {
+            manager = new Data_Manager();
+        }
+        return manager;
+    }
+
+    public List_Double_User getList_user() {
+        return list_user;
+    }
+
+    public Stack_Of_Product getStack_Product() {
+        return stack_Product;
+    }
 }
