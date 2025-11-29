27f2941ae3679599dded6a433620027fb0b762bb
Aileth Oquendo <ailethcantero@gmail.com>
2025-11-28 22:41:47 -0500

    Adding a wish list to the catalog panel.

--- src/Controllers/ComponentControllers/Controller_Product_Component.java
+++ src/Controllers/ComponentControllers/Controller_Product_Component.java
@@ -3,13 +3,9 @@ package Controllers.ComponentControllers;
 import Models.Product;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.fxml.*;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
 
 public class Controller_Product_Component implements Initializable {
 
